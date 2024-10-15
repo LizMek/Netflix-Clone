b58299044084db0e06aecc2d7b6020a432e111d4
LizMek <lizaassefa@gmail.com>
2024-10-15 17:38:47 -0400

    Add files via upload

---
+++ build/precache-manifest.2e0bb998d80a120c37dfca5c193e8293.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de67bdc71fc39a9845b04e3e6694f8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e89527eed81b29209f7",
+    "url": "/static/css/main.14604ab8.chunk.css"
+  },
+  {
+    "revision": "0da5170959d230648ab1",
+    "url": "/static/js/2.e674e902.chunk.js"
+  },
+  {
+    "revision": "98ba7388b8e6f4cfa8d1",
+    "url": "/static/js/3.34b8d668.chunk.js"
+  },
+  {
+    "revision": "5e89527eed81b29209f7",
+    "url": "/static/js/main.272aadee.chunk.js"
+  },
+  {
+    "revision": "d9f55312c8b8c072fe13",
+    "url": "/static/js/runtime~main.5b227698.js"
+  }
+]);
